1f417cfe30d966f4dd836ddd7a34f381850953d9
woyuen <wing-on@stelace.com>
2019-07-19 21:16:21 +0200

    fix: transaction asset isn't populated for conversations

--- src/store/inbox/getters.js
+++ src/store/inbox/getters.js
@@ -106,9 +106,24 @@ export function conversations (state, getters, rootState, rootGetters) {
       ratingsReadonly = true
     }
 
+    let transactionAsset
+    if (transactionId) {
+      transactionAsset = get(transaction, 'assetSnapshot', {})
+      const assetType = transaction.assetType
+
+      transactionAsset = populateAsset({
+        asset: transactionAsset,
+        usersById: {},
+        categoriesById,
+        assetTypesById: {
+          [transaction.assetTypeId]: assetType
+        }
+      })
+    }
+
     const conversation = {
       id: conversationId,
-      asset: transactionId ? get(transaction, 'assetSnapshot', {}) : asset,
+      asset: transactionId ? transactionAsset : asset,
       transaction,
       transactionActions,
       ratingsPrompt,
