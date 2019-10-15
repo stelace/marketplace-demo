1dcdbd0f08ec5f8d82349c85052b60931b312d33
woyuen <wing-on@stelace.com>
2019-10-15 13:00:21 +0200

    refactor(time): improve `convertEndDate` function

--- src/utils/time.js
+++ src/utils/time.js
@@ -22,9 +22,19 @@ function getQuasarTimeUnit (timeUnit) {
   return mapQuasarTimeUnitToStelace[timeUnit]
 }
 
+/**
+ * This function operates date manipulation on `endDate`, especially useful for one time unit length transactions
+ * e.g. one-day transaction on '2019-01-01' with `startDate` and `endDate` at '2019-01-01T00:00:00.000Z' from calendar
+ * it will transform `endDate` into '2019-01-02T00:00:00.000Z' if target equals to 'api'
+ * @param {String}  endDate
+ * @param {Object}  [options]
+ * @param {String}  [options.timeUnit = 'd'] - allowed values: `M`, `d`, `h`, `m` for months, days, hours and minutes
+ * @param {Boolean} [options.includeEndDate = true] - if true, an additional time unit will be added to `endDate`
+ * @param {String}  options.target - specify the conversion direction (from ui to api, or inversely)
+ */
 export function convertEndDate (endDate, { timeUnit = 'd', includeEndDate = true, target } = {}) {
-  const allowedTarget = ['api', 'ui']
-  if (!allowedTarget.includes(target)) throw new Error(`Invalid type: ${allowedTarget.join(', ')} expected`)
+  const allowedTargets = ['api', 'ui']
+  if (!allowedTargets.includes(target)) throw new Error(`Invalid target: ${allowedTargets.join(', ')} expected`)
 
   if (!includeEndDate) return endDate
 
