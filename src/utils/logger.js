85db148798d0b9e471b44907592f2aadf83c6271
Andy Bruère <andy@stelace.com>
2020-07-01 10:14:10 +0200

    perf: add error queue to lazyload sentry

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,19 @@
+import { getStelaceEnv } from 'src/utils/stelace'
 import EventBus from 'src/utils/event-bus'
-import { getRemoteLogger } from 'src/boot/errors'
+
+const stelaceEnv = getStelaceEnv()
+const sentryDSN = process.env.VUE_APP_SENTRY_LOGGING_DSN
+// Detect if we’re using Netlify branch deploy to set logging environment
+// https://www.netlify.com/docs/continuous-deployment/#environment-variables
+const branchDeploy = process.env.DEV && process.env.CONTEXT ? process.env.BRANCH : ''
+
+const errorQueue = []
+const messageQueue = []
+
+const sentryLogger = {
+  capture: err => { if (sentryDSN) errorQueue.push(err) },
+  message: err => { if (sentryDSN) messageQueue.push(err) },
+}
 
 /**
  * Send error to remote logging service or to console during development,
@@ -12,18 +26,20 @@ import { getRemoteLogger } from 'src/boot/errors'
  *   - or a valid translation content key String such as `error.failed_updoad`,
  *   - or a full options object expected by Notify mixin.
  * @param {Object} [options.level=error] - console level (error, info…)
+ * @param {Object} [options.isMessage=false] - should emit simple message instead of error
  */
 export default function logger (err, options = {}) {
   const level = options.level
-  const { capture } = getRemoteLogger()
+  const { capture, message } = sentryLogger
+  const log = options.isMessage ? message : capture
 
   /* eslint-disable no-console */
   if (!level || level === 'error') {
     if (process.env.DEV) console.error(err, options.data)
-    else capture(err)
+    else log(err)
   } else {
     if (process.env.DEV) console[level](err, options.data)
-    else capture(err)
+    else log(err)
   }
   /* eslint-enable no-console */
 
@@ -34,4 +50,53 @@ export default function logger (err, options = {}) {
   // Note that we have to pass notification set to true or to translation content key string
   // for a notification to show up, to avoid duplicate with some notification shown previously.
   EventBus.$emit('error', options)
+}
+
+export async function initSentry () {
+  // Must init sentry _after_ customizing Vue.config.errorHandler
+  // https://github.com/getsentry/sentry-javascript/blob/master/packages/integrations/src/vue.ts#L72
+  if (!process.env.DEV && sentryDSN) {
+    const [Sentry, { Vue: SentryVue }] = await Promise.all([
+      import(/* webpackChunkName: 'logging' */ '@sentry/browser'),
+      import(/* webpackChunkName: 'logging' */ '@sentry/integrations')
+    ])
+
+    Sentry.init({
+      dsn: sentryDSN,
+      environment: branchDeploy ? `${stelaceEnv}-${branchDeploy}` : stelaceEnv,
+      release: process.env.VUE_APP_GIT_COMMIT_SHA,
+      integrations: [
+        new SentryVue({
+          attachProps: false, // disabling for privacy
+        }),
+      ],
+      // https://github.com/quasarframework/quasar/issues/2233
+      ignoreErrors: ['ResizeObserver loop']
+    })
+
+    Sentry.configureScope((scope) => {
+      scope.setTag('stelaceEnv', stelaceEnv)
+    })
+
+    sentryLogger.capture = err => {
+      if (err.message && err.message.toLowerCase().includes('user session expired')) return
+
+      Sentry.captureException(err)
+    }
+    sentryLogger.message = msg => Sentry.captureMessage(msg)
+
+    // Now we can dequeue and send errors
+    ;(async () => {
+      if (errorQueue.length) EventBus.$emit('error')
+
+      while (errorQueue.length) {
+        sentryLogger.capture(errorQueue.shift())
+        await new Promise(resolve => setTimeout(resolve, 100))
+      }
+      while (messageQueue.length) {
+        sentryLogger.capture(messageQueue.shift())
+        await new Promise(resolve => setTimeout(resolve, 100))
+      }
+    })()
+  }
 }
